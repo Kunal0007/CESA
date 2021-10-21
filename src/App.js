d3c09be9dbd2821aec33e386e910bd9fcbb4dca8
Kunal Patil <73381050+Kunal0007@users.noreply.github.com>
2021-10-21 23:42:21 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const Add = (product) => {
-    const exist = cart.find((x) => x.id === product.id);
-    if (exist) {
+    const oldcart = cart.find((x) => x.id === product.id);
+    if (oldcart) {
       setCart(
         cart.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+          x.id === product.id ? { ...oldcart, qty: oldcart.qty + 1, total: oldcart.total + product.price } : x
         )
       );
     } else {
-      setCart([...cart, { ...product, qty: 1 }]);
+      setCart([...cart, { ...product, qty: 1, total: product.price }]);
     }
   };
 
   const Remove = (product) => {
-    const exist = cart.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
+    const oldcart = cart.find((x) => x.id === product.id);
+    if (oldcart.qty === 1) {
       setCart(cart.filter((x) => x.id !== product.id));
     } else {
       setCart(
         cart.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+          x.id === product.id ? { ...oldcart, qty: oldcart.qty - 1, total: oldcart.total - product.price } : x
         )
       );
     }
@@ -40,7 +40,7 @@ function App() {
   return (
     <div className="App">
       <div className="heading">
-        <h1>Shop</h1>
+        <h1>Fruit Shop</h1>
       </div>
       <div className="home">
         <Products products={products} add={Add} remove={Remove}/>
